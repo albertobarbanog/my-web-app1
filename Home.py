3d4b6d3b36cc6ac34bb99aa613cb3a789cfe146d
albertobarbanog <albertobarbanog@gmail.com>
2023-12-21 14:25:22 -0300

    Finished experiment

--- Home.py
+++ Home.py
@@ -3,6 +3,13 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(
+    page_title="My Todo App",
+    page_icon="✅",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -10,7 +17,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="Enter a new todo", placeholder="e.g. Buy milk...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,5 +31,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a new todo", placeholder="e.g. Buy milk...",
-              on_change=add_todo, key='new_todo')
